9ac01adf077ad0acdbd70e4a7354308be319bb2e
Grave <pmpiga1@gmail.com>
2023-03-07 14:13:06 -0500

    add editing ability

--- src/csvtbl.nim
+++ src/csvtbl.nim
@@ -1,4 +1,5 @@
 import std/sequtils
+import std/strutils
 import std/os
 
 import csvtools
@@ -14,6 +15,16 @@ if paramCount() < 1:
 var 
   filename = paramStr(1)
   rows = csvRows(filename).toSeq()
+  autoSave: bool
+
+proc saveFile =
+  var writeStr: string
+
+  for csvRow in rows:
+    writestr.add csvRow.join(",")
+    writeStr.add '\n'
+
+  filename.writeFile(writeStr)
 
 proc modelNumColumns(mh: ptr TableModelHandler, m: ptr rawui.TableModel): cint {.cdecl.} = cint len(csvRows(filename).toSeq()[0])
 proc modelNumRows(mh: ptr TableModelHandler, m: ptr rawui.TableModel): cint {.cdecl.} = cint len(csvRows(filename).toSeq()) - 1
@@ -30,12 +41,21 @@ proc modelCellValue(mh: ptr TableModelHandler, m: ptr rawui.TableModel, row, col
       return newTableValue(val[col - 1]).impl
 
 proc modelSetCellValue(mh: ptr TableModelHandler, m: ptr rawui.TableModel, row, col: cint, val: ptr rawui.TableValue) {.cdecl.} =
-  discard # For now...
+  rows[row + 1][col - 1] = $rawui.tableValueString(val)
+
+  if autoSave:
+    saveFile()
 
 proc main =
   var window: Window
 
   let fileMenu = newMenu("File")
+
+  fileMenu.addItem("Save") do (m: MenuItem, win: Window):
+    saveFile()
+
+  fileMenu.addCheckItem("Enable AutoSave") do (m: MenuItem, win: Window):
+    autoSave = m.checked
 
   fileMenu.addQuitItem() do () -> bool:
     window.destroy()
@@ -69,7 +89,7 @@ proc main =
   table.addTextColumn("Row", 0, TableModelColumnNeverEditable)
 
   for idx, header in csvRows(filename).toSeq()[0]:
-    table.addTextColumn(header, idx + 1, TableModelColumnNeverEditable)
+    table.addTextColumn(header, idx + 1, TableModelColumnAlwaysEditable)
 
   box.add table, true
 
